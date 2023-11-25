3063b9d5c8eaac8fe96280e13d9fcb87f5631360
playerandplayer <oscar.dk.roskilde@gmail.com>
2023-11-25 23:51:25 +0100

    push for lives in

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Sample list of countries
+    // Sample list of countries with image URLs
     const countries = [
-        { name: "United States", capital: "Washington, D.C.", population: "331 million" },
-        { name: "United Kingdom", capital: "London", population: "68 million" },
-        { name: "Canada", capital: "Ottawa", population: "38 million" },
+        { name: "United States", capital: "Washington, D.C.", population: "331 million", image: "usa.jpg" },
+        { name: "United Kingdom", capital: "London", population: "68 million", image: "uk.jpg" },
+        { name: "Canada", capital: "Ottawa", population: "38 million", image: "canada.jpg" },
         // Add more countries as needed
     ];
 
-    // Populate the country list
     const countryList = document.getElementById("countries");
+    const selectedCountryDiv = document.getElementById("selected-country");
+
     countries.forEach(country => {
         const listItem = document.createElement("li");
         listItem.textContent = country.name;
@@ -16,13 +17,12 @@ document.addEventListener("DOMContentLoaded", function () {
         countryList.appendChild(listItem);
     });
 
-    // Display country information when a country is selected
     function showCountryInfo(country) {
-        const selectedCountryDiv = document.getElementById("selected-country");
         selectedCountryDiv.innerHTML = `
             <strong>Name:</strong> ${country.name}<br>
             <strong>Capital:</strong> ${country.capital}<br>
-            <strong>Population:</strong> ${country.population}
+            <strong>Population:</strong> ${country.population}<br>
+            <img src="${country.image}" alt="${country.name}" width="100">
         `;
     }
 });
